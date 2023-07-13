3e84777157b490239cc2ce8142e03d00eb0f7115
Lance Matthew Rivera <lance@lances-air.lan>
2023-07-14 00:32:21 +0800

    feat: added some unit tests for get_visitor_count

---
+++ tests/unit/test_get_visitor_count.py
@@ -0,0 +1,29 @@
+import json
+import pytest
+import sys
+
+sys.path.insert(0, "/Users/lance/Downloads/resume_backend/lambdas")
+
+from get_visitor_count import app
+
+@pytest.fixture
+
+def get_event():
+    json_event = open("/Users/lance/Downloads/resume_backend/events/get_event.json", "r")
+    response = app.lambda_handler(json_event,"")
+    response_body = json.loads(response['body'])
+    viewer_count = int(response_body['Item']['viewer_count'])
+    return viewer_count
+
+def test_if_visitor_count_is_integer(get_event):
+    assert type(get_event) is int, "Could not convert value to int!"
+
+def test_if_visitor_count_is_positive(get_event):
+    assert get_event >= 0, "Count is negative!"
+
+def test_if_visitor_count_does_not_update_when_called_repeatedly(get_event):
+    original_value = get_event
+    i = 0
+    while (i < 5):
+        assert get_event == original_value, "Not supposed to change!"
+        i += 1
